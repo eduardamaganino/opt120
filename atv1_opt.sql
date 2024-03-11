bbc0f8ce3793ef61d18ecabdf56d52e55b3c45b8
Eduarda Vaz Maganino <98330048+eduardamaganino@users.noreply.github.com>
2024-03-11 20:51:35 -0300

    atividade 1-script sql

---
+++ atv1_opt.sql
@@ -0,0 +1,26 @@
+USE opt120;
+
+CREATE TABLE Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    senha VARCHAR(100)
+);
+
+CREATE TABLE Atividade(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(100),
+    descricao VARCHAR(100),
+    nota FLOAT,
+    data_atv DATETIME
+);
+
+CREATE TABLE Usuario_Atividade(
+    usuario_id INT,
+    atividade_id INT,
+    entrega DATETIME,
+    nota FLOAT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (atividade_id) REFERENCES atividade(id),
+    PRIMARY KEY (usuario_id, atividade_id)
+);
